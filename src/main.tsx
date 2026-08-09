b5ba34f94af0724040a7c2c42a779c7ccf9bb44e
ljkudo <lynnlee1358@gmail.com>
2026-08-09 15:44:04 +0700

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CatonahatHome from "./CatonahatHome";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CatonahatHome />
+  </React.StrictMode>
+);
